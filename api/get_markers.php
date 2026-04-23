906b4dad27ca4557b8200cec2ee546883bd1cd28
Staichi54 <73539099+Staichi54@users.noreply.github.com>
2026-04-23 15:06:41 -0500

    borrar directorio api
    pq voy a organizar jsjsj

--- api/get_markers.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once '../config.php';
-
-$stmt = $pdo->query("SELECT id, nombre, lat, lng, cantidad FROM marcadores");
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
