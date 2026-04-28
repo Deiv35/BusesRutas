1cabf5c07f87ff1f0ffded7520d3d2cfce8e05d0
Staichi54 <73539099+Staichi54@users.noreply.github.com>
2026-04-27 23:37:35 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,14 @@
 <?php
-$serverName = "DESKTOP-56FEFQK";
-$database = "BusesRemake";
+$serverName = "db28471.public.databaseasp.net";
+$database = "db28471";
+$user = "db28471";
+$password = "2Fb%y9-EH_z7";
 
 try {
     $conexion = new PDO(
-        "sqlsrv:Server=$serverName;Database=$database",
-        null,
-        null
+        "sqlsrv:Server=$serverName;Database=$database;Encrypt=true;TrustServerCertificate=true;MultipleActiveResultSets=true",
+        $user,
+        $password
     );
 
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
